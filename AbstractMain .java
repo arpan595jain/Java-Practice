78f0c12d61b315e6b3d4a9a5b4a4a441d4ff0383
Arpan Jain <arpanjain595@gmail.com>
2024-07-18 09:58:46 +0530

    AbstractMain

---
+++ AbstractMain .java
@@ -0,0 +1,14 @@
+abstract class Bike 
+{ Bike() 
+{ System.out.println("bike is created"); } 
+abstract void run(); 
+void changeGear() 
+{ System.out.println("gear changed"); } } 
+class Honda extends Bike 
+{ void run() 
+{ System.out.println("running safely.."); } } 
+public class AbstractMain 
+{ public static void main(String args[]) 
+{ Bike obj = new Honda(); 
+obj.run(); 
+obj.changeGear(); } }
