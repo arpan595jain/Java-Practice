cf3023332d6b7bdef3eb1d06b9d9d1c0e611bd20
Arpan Jain <arpanjain595@gmail.com>
2024-08-06 08:24:38 +0530

    Hashset

---
+++ Hashset.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+  public class Exercise1 {
+  public static void main(String[] args) {
+     // Create a empty hash set
+     HashSet<String> h_set = new HashSet<String>();
+   // use add() method to add values in the hash set
+          h_set.add("Red");
+          h_set.add("Green");
+          h_set.add("Black");
+          h_set.add("White");
+          h_set.add("Pink");
+          h_set.add("Yellow");
+   // print the hash set
+   System.out.println("The Hash Set: " + h_set); }}
