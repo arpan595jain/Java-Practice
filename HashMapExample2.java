7107a943ae1762cc878ddb232fbfaa0446ac235d
Arpan Jain <arpanjain595@gmail.com>
2024-08-08 14:37:17 +0530

    HashMapExample2

---
+++ HashMapExample2.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+public class Example2 {  
+   public static void main(String args[]){  
+  HashMap<Integer,String> hash_map= new HashMap<Integer,String>();  
+  hash_map.put(1, "Red");
+  hash_map.put(2, "Green");
+  hash_map.put(3, "Black");
+  hash_map.put(4, "White");
+  hash_map.put(5, "Blue");
+  System.out.println("Size of the hash map: "+hash_map.size());
+ }}
