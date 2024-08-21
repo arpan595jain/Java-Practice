1d80c31370e54d1988d61f18e8f42b49d249700b
Arpan Jain <arpanjain595@gmail.com>
2024-08-21 08:05:50 +0530

    Create Static Initializer

---
+++ Static Initializer.java
@@ -0,0 +1,30 @@
+ public static int B;
+    public static int H;
+    public static boolean flag;
+    
+    static
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        B = sc.nextInt();
+        H = sc.nextInt();
+        
+        if(B<=0 || H<=0)
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        else
+            flag = true;
+    }
+    
+
+
+
+public static void main(String[] args){
+if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
