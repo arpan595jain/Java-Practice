924c3881b12caeedf533f7067f70b9085bf0e73d
Arpan Jain <arpanjain595@gmail.com>
2024-08-30 13:53:48 +0530

    Create String Tokens

---
+++ String Tokens.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            if (!sc.hasNext()){
+        System.out.println(0);
+    }else {
+         String input=sc.nextLine();
+        String[]a = input.trim().split("[ !,?._'@]+");
+        ArrayList<String>listOfStrings =new ArrayList<String>(Arrays.asList(a));
+        System.out.println(listOfStrings.size());
+
+      for(String str:listOfStrings){
+        System.out.println(str);
+      }    
+    }
+
+    }
+}
+
