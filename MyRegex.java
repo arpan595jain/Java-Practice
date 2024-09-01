927a63f0d09476d5c6319f7d32519b6e1dbd9aaa
Arpan Jain <arpanjain595@gmail.com>
2024-09-01 08:59:47 +0530

    Create MyRegex

---
+++ MyRegex.java
@@ -0,0 +1,8 @@
+class MyRegex {
+    
+        
+    String pola = "([01]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])";
+    String pattern = "^" + pola + "\\." + pola + "\\." + pola + "\\." + pola + "$";
+    
+    
+    }
